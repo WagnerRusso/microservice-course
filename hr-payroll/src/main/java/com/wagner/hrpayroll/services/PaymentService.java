a87573f90b38ed7ca88c77fa7c31150558c1844d
wrusso <wrusso@interaxa.com>
2021-09-20 17:39:41 -0300

    hr-pyroll Feign Client

--- hr-payroll/src/main/java/com/wagner/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/wagner/hrpayroll/services/PaymentService.java
@@ -1,32 +1,24 @@
 package com.wagner.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.wagner.hrpayroll.entities.Payment;
 import com.wagner.hrpayroll.entities.Worker;
+import com.wagner.hrpayroll.feignclients.WorkerFeingClient;
 
 @Service
 public class PaymentService {
 
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
+
 	@Autowired
-	private RestTemplate restTemplate;
-	
+	private WorkerFeingClient workerFeingClient;
 	
 	public Payment getPaymente(long workedId, int days) {
 		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workedId);
+		Worker worker = workerFeingClient.findById(workedId).getBody();
 		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
 		return new Payment(worker.getName(),worker.getDailyIncome(), days);
 	}
 
